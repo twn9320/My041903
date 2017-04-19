49cbeff998061e8aa0b7206c00b763b24a86e434
twn9320 <twn9320@gmail.com>
2017-04-19 14:20:21 +0800

    PreferenceActivity(加入Account)

--- app/src/main/java/com/example/yvtc/my041903/SettingsActivity.java
+++ app/src/main/java/com/example/yvtc/my041903/SettingsActivity.java
@@ -178,6 +178,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // updated to reflect the new value, per the Android Design
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("account"));
             bindPreferenceSummaryToValue(findPreference("example_list"));
         }
 
