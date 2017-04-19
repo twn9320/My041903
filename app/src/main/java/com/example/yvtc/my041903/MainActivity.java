74e61a61c711579359d66a42d6aa4f83fc67eb87
twn9320 <twn9320@gmail.com>
2017-04-19 13:23:48 +0800

    多write

--- app/src/main/java/com/example/yvtc/my041903/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my041903/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +17,25 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sp = getSharedPreferences("mydata",MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("username","Peter");
-        editor.commit();
+        String str = sp.getString("username","");
+        EditText ed = (EditText)findViewById(R.id.editText);
+        ed.setText(str);
+
     }
 
     public void clickRead(View v){
         SharedPreferences sp =getSharedPreferences("mydata",MODE_PRIVATE);
         String str = sp.getString("username","");
-        Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT).show();
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText(str);
+    }
+
+    public void clickWrite(View v){
+        SharedPreferences sp = getSharedPreferences("mydata",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        EditText ed = (EditText)findViewById(R.id.editText);
+        editor.putString("username",ed.getText().toString());
+        editor.commit();
     }
 }
